5bf0e6c700ca448c541f7fbe89811b20ef4ca643
qiukai <513862058@qq.com>
2018-10-16 11:54:38 +0800

    init

---
+++ src/packages/tree/tree.vue
@@ -0,0 +1,15 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import treeNode from './tree-node.vue'
+
+@Component({
+  components: {
+  treeNode
+  },
+  })
+export default class App extends Vue {}
+</script>
